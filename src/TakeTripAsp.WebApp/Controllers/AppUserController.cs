99abbab50b39c58aad8dc7fe56ae866d3da68bec
SmotrUliana <uliana.smotr@oa.edu.ua>
2023-09-20 05:29:31 +0300

    Create CRUD for AppUser

---
+++ src/TakeTripAsp.WebApp/Controllers/AppUserController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TakeTripAsp.Core.Entity;
+using TakeTripAsp.Repository;
+
+namespace TakeTripAsp.WebApp.Controllers
+{
+    public class AppUserController : Controller
+    {
+        public readonly IRepository<AppUser, int> repository;
+
+        public AppUserController(IRepository<AppUser, int> repository)
+        {
+            this.repository = repository;
+        }
+        public IActionResult Index()
+        {
+            return View(repository.GetAll());
+        }
+
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(AppUser model)
+        {
+            if (ModelState.IsValid)
+            {
+                var appUser = new AppUser { Name = model.Name };
+                repository.Create(appUser);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int id)
+        {
+            return View(repository.Get(id));
+        }
+
+        [HttpPost]
+        public IActionResult Delete(AppUser appUser)
+        {
+            repository.Delete(appUser);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return View(repository.Get(id));
+        }
+
+        [HttpPost]
+        public IActionResult Edit(AppUser appUser)
+        {
+            repository.Update(appUser);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
--- src/TakeTripAsp.WebApp/Program.cs
+++ src/TakeTripAsp.WebApp/Program.cs
@@ -18,7 +18,9 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddScoped<IRepository<Status, int>, Repository< Status, int>>();
 builder.Services.AddScoped<IRepository<BookingStatus, int>, Repository< BookingStatus, int>>();
-builder.Services.AddScoped<IRepository<Category, int>, Repository< Category, int>>();
+builder.Services.AddScoped<IRepository<Category, int>, Repository< Category, int>>(); 
+builder.Services.AddScoped<IRepository<AppUser, int>, Repository<AppUser, int>>();
+
 
 
 var app = builder.Build();
