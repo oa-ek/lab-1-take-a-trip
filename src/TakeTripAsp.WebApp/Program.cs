ba04da85f839844ef788fcdc9998bcadcdc5bcee
SmotrUliana <uliana.smotr@oa.edu.ua>
2023-09-21 23:35:35 +0300

    Create CRUD for SelectedTour

--- src/TakeTripAsp.WebApp/Program.cs
+++ src/TakeTripAsp.WebApp/Program.cs
@@ -24,6 +24,7 @@ builder.Services.AddScoped<IRepository<Tour, int>, Repository<Tour, int>>();
 builder.Services.AddScoped<IRepository<Profile, int>, Repository<Profile, int>>();
 builder.Services.AddScoped<IRepository<Bookings, int>, Repository<Bookings, int>>();
 builder.Services.AddScoped<IRepository<Reviews, int>, Repository<Reviews, int>>();
+builder.Services.AddScoped<IRepository<SelectedTour,int>, Repository<SelectedTour, int>>();
 
 
 
