cb5ef09a8de9d7a45bf7e2aa5206fc3bc13f96c2
SmotrUliana <uliana.smotr@oa.edu.ua>
2023-10-16 21:31:45 +0300

    modify user

--- src/TakeTripAsp.Core/Entity/AppUser.cs
+++ src/TakeTripAsp.Core/Entity/AppUser.cs
@@ -1,9 +1,20 @@
-﻿namespace TakeTripAsp.Core.Entity
-{
-    public class AppUser : BaseEntity
-    {
-        public required string Name { get; set; }
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace TakeTripAsp.Core.Entity
+{
+    public class AppUser : IdentityUser
+    {
+        public  string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public  string? Email { get; set; }
+       // public required string Bio { get; set; }
+        //public string? CoverPath { get; set; } = "\\img\\user\\user.jpg";
+
+       // [NotMapped]
+       // public IFormFile? CoverFile { get; set; }
+        public DateTime? DateOfBirth { get; set; }
         public virtual ICollection<SelectedTour>? Tours { get; set; }
     }
 }
