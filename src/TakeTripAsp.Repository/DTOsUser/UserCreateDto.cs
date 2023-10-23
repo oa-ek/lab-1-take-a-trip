da656ee645d642325d4611552c5f55d1ea31ef99
SmotrUliana <uliana.smotr@oa.edu.ua>
2023-10-23 09:42:18 +0300

    creation reposetory for user

---
+++ src/TakeTripAsp.Repository/DTOsUser/UserCreateDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakeTripAsp.Repository.DTOsUser
+{
+    public class UserCreateDto
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
+
+
+
+   
---
+++ src/TakeTripAsp.Repository/Interfaces/IUserRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TakeTripAsp.Repository.DTOsUser;
+//using TakeTripAsp.Core.Entity;
+
+namespace TakeTripAsp.Repository.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<UserDto> GetAsync(string id);
+        Task<IEnumerable<UserDto>> GetAllAsync(); //AppUser
+        Task<string> CreateAsync(UserCreateDto obj);
+        Task UpdateAsync(UserDto obj, string[] roles);
+        Task<IEnumerable<string>> GetRolesAsync();
+        Task DeleteAsync(string id);
+    }
+}
+
+
