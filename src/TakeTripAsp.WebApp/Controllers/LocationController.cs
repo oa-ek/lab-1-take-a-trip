f7712e766ec0bfc34cf73081389c41668388d514
Nastya <anastasiia.havrylchyk@oa.edu.ua>
2023-11-02 21:50:51 +0200

    add microsoft bing maps

---
+++ src/TakeTripAsp.WebApp/Controllers/LocationController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TakeTripAsp.WebApp.Models;
+
+namespace TakeTripAsp.WebApp.Controllers
+{
+    public class LocationController : Controller
+    {
+        public IActionResult Index()
+        {
+            LocationLists model = new LocationLists();
+            var locations = new List<Locations>()
+            {
+                new Locations(1, "Bhubaneswar","Bhubaneswar, Odisha", 20.296059, 85.824539),
+                new Locations(2, "Hyderabad","Hyderabad, Telengana", 17.387140, 78.491684),
+                new Locations(3, "Bengaluru","Bengaluru, Karnataka", 12.972442, 77.580643)
+            };
+            model.LocationList = locations;
+            return View(model);
+        }
+    }
+}
