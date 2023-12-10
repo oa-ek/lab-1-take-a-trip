aa77fd2575e53606846f3e612b2497f5064c6dd9
SmotrUliana <uliana.smotr@oa.edu.ua>
2023-12-10 04:00:16 +0200

    Creating a clean architecture for the Country, as well as creating the CountryСontroller

---
+++ src/TakeATripAsp.Application/Features/CountryFeatures/CountryMapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using TakeTripAsp.Application.Features.CountryFeatures.CountryDtos;
+using TakeTripAsp.Domain.Entity;
+
+namespace TakeTripAsp.Application.Features.CountryFeatures
+{
+    public class CountryMapper: Profile
+    {
+        public CountryMapper()
+        {
+            CreateMap<Country, CreateCountryDto>();
+            CreateMap<CreateCountryDto, Country>();
+
+            CreateMap<Country, ReadCountryDto>();
+            CreateMap<ReadCountryDto, Country>();
+        }
+    }
+}
---
+++ src/TakeATripAsp.Application/Features/CountryFeatures/Queries/GetAllCountry/GetAllCountryQueriesHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using MediatR;
+using TakeTripAsp.Application.Features.CountryFeatures.CountryDtos;
+using TakeTripAsp.Application.Repository;
+using TakeTripAsp.Domain.Entity;
+
+namespace TakeTripAsp.Application.Features.CountryFeatures.Queries.GetAllCountry
+{
+    public class GetAllCountryQueriesHandler
+   : IRequestHandler<GetAllCountryQueries, IEnumerable<ReadCountryDto>>
+    {
+        protected readonly IBaseRepository<Country, int>? _countryRepository;
+        protected readonly IMapper _mapper;
+
+        public GetAllCatagoryQueriesHandler(
+            IBaseRepository<Country, int> countryRepository,
+            IMapper mapper)
+        {
+            (_countryRepository, _mapper) = (countryRepository, mapper);
+        }
+
+        public async Task<IEnumerable<ReadCountryDto>> Handle(
+            GetAllCountryQueries request,
+            CancellationToken cancellationToken)
+        {
+            return _mapper.Map<IEnumerable<Country>,
+                IEnumerable<ReadCountryDto>>(await _countryRepository.GetAllAsync());
+        }
+    }   
+}
+
+
